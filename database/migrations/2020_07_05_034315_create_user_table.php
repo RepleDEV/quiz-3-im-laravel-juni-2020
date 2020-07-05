a937c5fe97247114baf20ea3d3955c0ca7fa59c5
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-07-05 11:29:26 +0700

    commit

---
+++ database/migrations/2020_07_05_034315_create_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name',255);
+            $table->string('user_email', 255);
+            $table->string('user_passwd', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
